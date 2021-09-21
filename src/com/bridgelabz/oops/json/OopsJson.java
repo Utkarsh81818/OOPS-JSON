6cf5f6435a2cacfa81e5fe9c33e84818cf83965b
Utkarsh Mishra <utkarshkr945@gmail.com>
2021-09-21 19:03:12 +0530

    Added: Inventory Details Using JSON File

--- src/com/bridgelabz/oops/json/OopsJson.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.oops.json;
-
-public class OopsJson {
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to OOPS problem using JSON ");
-    }
-}
